ffe792d3ef09bf9e0442e5c8bf31edcef4134fc8
Atul Singh <atuls878@gmail.com>
2024-02-04 17:34:24 +0530

    fourth

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import "dotenv/config";
 const app = express();
 const port = process.env.PORT ;//|| "3000"
 
-app.use(express.static("dist"));
+// app.use(express.static("dist"));
 app.use(bodyParser.json());
 // app.use(
 //   cors({
