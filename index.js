08648b40861aa7abbae701e0e64e1ee4bb6a7096
Atul Singh <atuls878@gmail.com>
2024-02-05 01:06:27 +0530

    m7

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import cors from "cors";
 
 const app = express();
 app.use(cors());
-const port = process.env.PORT; //|| "3000"
+const port = process.env.PORT || "3000"; //
 
 app.use(bodyParser.json());
 
